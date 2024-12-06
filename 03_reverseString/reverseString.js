39d663657acc9a3a662f47dcf7fc11a75d87c7bb
chart5766 <8448700+chart5766@users.noreply.github.com>
2024-12-05 23:02:47 -0600

    03 exercise is finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
-};
+    let x = string.split("");
+    let x_array = x.reverse();
+    let x_reverse = x_array.join("")
+    return x_reverse
+
+ };
 
 // Do not edit below this line
 module.exports = reverseString;
